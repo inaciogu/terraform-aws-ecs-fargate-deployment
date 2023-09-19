8d29b4e47a9a50d214f7215d7666634e97423057
gustavo <gus.inacio@hotmail.com>
2023-09-19 12:22:35 -0300

    chore: update variables

--- variables.tf
+++ variables.tf
@@ -1,27 +1,50 @@
 variable "aws_access_key_id" {
   description = "value of AWS_ACCESS_KEY_ID"
+  type        = string
 }
 
 variable "aws_secret_access_key" {
   description = "value of AWS_SECRET_ACCESS_KEY"
+  type        = string
 }
 
 variable "aws_region" {
   description = "value of AWS_DEFAULT_REGION"
+  type        = string
+}
+
+variable "account_id" {
+  description = "value of AWS account id"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks of the public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks of the private subnets"
+  type        = list(string)
 }
 
 variable "clusters" {
   description = "A list of ecs clusters to create with configurations"
   type = list(object({
-    name          = string # Name of the cluster
+    name = string # Name of the cluster
     services = list(object({
       name = string # Name of the service
       task_definition = object({
         family_name = string # Name of the task definition family
         container_definitions = list(object({
-          name  = string # Name of the container
-          repository_name = string # Name of ECR repository to used
-          dockerfile_location = optional(string) # Location of the Dockerfile
+          name                = string # Name of the container
+          repository_name     = string # Name of ECR repository to used
+          dockerfile_location = string # path to the Dockerfile
           portMappings = optional(list(object({
             containerPort = number # Port of the container
             hostPort      = number # Port of the host
@@ -30,12 +53,11 @@ variable "clusters" {
           environment = optional(list(object({
             name  = string # Name of the environment variable
             value = string # Value of the environment variable
-          }))) # Environment variables
+          })))             # Environment variables
           secrets = optional(list(object({
             name      = string # Name of the secret
             valueFrom = string # ARN of the secret
           })))
-          log_group_name = string # Name of the log group
         }))
         cpu    = number # CPU units
         memory = number # Memory units
