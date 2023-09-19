3829560bd1770a04240c2d31df696d679ab34ea4
gustavo <gus.inacio@hotmail.com>
2023-09-19 12:21:18 -0300

    chore: global network configuration

--- network.tf
+++ network.tf
@@ -1,43 +1,50 @@
 resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  count = 2
-  vpc_id = aws_vpc.example_vpc.id
-  cidr_block = "10.0.${count.index}.0/24"
-  availability_zone = "us-east-1a"
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  count = 2
-  vpc_id = aws_vpc.example_vpc.id
-  cidr_block = "10.0.${count.index + 2}.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
-resource "aws_internet_gateway" "example_igw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.example_vpc.id
 }
 
 resource "aws_route_table_association" "public_association" {
-  count          = 2
-  subnet_id      = aws_subnet.public_subnet_1[count.index].id
+  count = length(var.public_subnet_cidr_blocks)
+
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_vpc.example_vpc.default_route_table_id
 }
 
 resource "aws_route" "public_route" {
-  count             = 2
-  route_table_id    = aws_vpc.example_vpc.default_route_table_id
+  route_table_id         = aws_vpc.example_vpc.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id        = aws_internet_gateway.example_igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_security_group" "test-group" {
-  name = "teste-group"
+resource "aws_security_group" "ecs" {
+  name        = "test-group"
   description = "Allow all inboud and outbound traffic"
-  vpc_id = aws_vpc.example_vpc.id
+  vpc_id      = aws_vpc.example_vpc.id
+  depends_on  = [aws_vpc.example_vpc]
 
   egress {
     from_port   = 0
