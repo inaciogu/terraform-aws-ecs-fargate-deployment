b1422902a04b4bfa30526c6ea1ae4a61370c239b
Gustavo Inacio <gus.inacio@hotmail.com>
2023-10-03 10:48:07 -0300

    chore: update example

--- example/main.tf
+++ example/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  region     = "us-east-1"
-  secret_key = ""
+  region = "us-east-1"
+
   access_key = ""
+  secret_key = ""
 }
 
 module "ecs-fargate" {
-  source                = "../"
-  aws_access_key_id     = ""
-  aws_secret_access_key = ""
-  aws_region            = ""
-  account_id            = ""
+  source     = "../"
+  aws_region = "us-east-1"
+  account_id = "123456789012"
 
   vpc_cidr_block             = "10.0.0.0/16"
   public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -19,17 +18,44 @@ module "ecs-fargate" {
   clusters = [{
     name = "example-cluster"
     services = [{
-      desired_count = 1
-      name          = "example-service"
+      desired_count             = 1
+      name                      = "example-service"
+      enable_queue_auto_scaling = true
+      auto_scaling = {
+        min_capacity = 1
+        max_capacity = 10
+        queue_name   = "example-queue"
+        steps = [{
+          lower_bound = 1
+          upper_bound = 2
+          change      = 1
+          },
+          {
+            lower_bound = 2
+            upper_bound = 3
+            change      = 2
+          },
+          {
+            lower_bound = 3
+            upper_bound = 4
+            change      = 3
+          },
+          {
+            lower_bound = 4
+            change      = 4
+          }
+        ]
+      }
       task_definition = {
-        cpu         = 1024
-        memory      = 2048
+        cpu         = 512
+        memory      = 1024
         family_name = "example-task"
         container_definitions = [
           {
             name                    = "example-container"
             repository_name         = "example-repository"
-            create_repository_setup = false
+            create_repository_setup = true
+            dockerfile_location     = "."
             portMappings = [{
               containerPort = 80
               hostPort      = 80
